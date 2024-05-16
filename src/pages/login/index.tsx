7b2238c277e1d16d7f01eb68ed0ea6b687b5b14e
Sadew Hiruditha <sadewhiruditha1@gmail.com>
2024-05-16 09:11:46 +0530

    refactor: Update login page layout and responsiveness

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Image, Stack, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Stack, Text, useBreakpointValue, Heading } from "@chakra-ui/react";
 import React from "react";
 import { LoginForm } from "./login_form";
 import { colors } from "theme/colors";
@@ -10,18 +10,30 @@ const Login: React.FC = () => {
     console.log('Login clicked');
     navigate('/register'); // Navigate to the next page after login
   };
+
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
+
   return (
-    <Flex direction="row" minH="100vh">
-      <Box w="50%" bgColor={colors.primary[100]}>
-        <Stack direction={"column"} align="center" justify="center" h="100%" spacing={4}>
-          <Image src="/images/L01.svg" w={300} />
-          <Text fontSize="4xl" fontWeight="400" color={"black"}>Drop Here</Text>
-          <Text fontSize="14px" fontWeight="300" color={"gray.600"} maxW="50%" textAlign="center">
-            Lorem ipsum dolor sit consecteturis amet, consecteturis dolor sit amet
-          </Text>
-        </Stack>
-      </Box>
-      <LoginForm onLogin={handleLogin} />
+    <Flex direction="column" minH="100vh">
+      {!isLargeScreen && (
+        <Flex direction="row" bg={colors.primary[100]} p="5">
+          <Heading m={"auto"}>Drop Here</Heading>
+        </Flex>
+      )}
+      <Flex direction="row" flexGrow={1}>
+        {isLargeScreen && (
+          <Box w="50%" bgColor={colors.primary[100]}>
+            <Stack direction={"column"} align="center" justify="center" h="100%" spacing={4}>
+              <Image src="/images/L01.svg" w={300} />
+              <Text fontSize="4xl" fontWeight="400" color={"black"}>Drop Here</Text>
+              <Text fontSize="14px" fontWeight="300" color={"gray.600"} maxW="50%" textAlign="center">
+                Lorem ipsum dolor sit consecteturis amet, consecteturis dolor sit amet
+              </Text>
+            </Stack>
+          </Box>
+        )}
+        <LoginForm onLogin={handleLogin} />
+      </Flex>
     </Flex>
   );
 };
