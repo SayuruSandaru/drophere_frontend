32c6307a82dc4779bfcbb3daa082b06dda4558ee
sayurusandaru <sayurusadaru12@gmail.com>
2024-06-19 18:47:51 +0530

    refactor: Update resultcard component

--- src/pages/order - delivery/index.tsx
+++ src/pages/order - delivery/index.tsx
@@ -1,6 +1,6 @@
 //import React, { useState, MouseEvent } from 'react';
-    // import { Box, Button, FormControl, FormLabel, Input, HStack, VStack, Text, Stack, useDisclosure } from '@chakra-ui/react';
-    // import 'react-datepicker/dist/react-datepicker.css';
+// import { Box, Button, FormControl, FormLabel, Input, HStack, VStack, Text, Stack, useDisclosure } from '@chakra-ui/react';
+// import 'react-datepicker/dist/react-datepicker.css';
 import React from "react";
 import { Box, Flex, Heading, VStack, Input, Button, Icon, Image, Text } from "@chakra-ui/react";
 //import { RouterPaths } from "router/routerConfig";
@@ -8,151 +8,137 @@ import { FaMapMarkerAlt, FaUser } from "react-icons/fa";
 
 
 const OrderDelivery = () => {
-  
-  
+
+
 
   return (
-    <Flex
-      direction="column"
-      bg="gray.200"
-      p={6}
-      alignItems="center"
+    <Box>
+      <Flex
+        direction="column"
+        bg="gray.100"
+        p={6}
+        alignItems="center"
 
-    >
-      <Heading as="h1" size="lg" textAlign="center" mb={6}>
-        Review your delivery
-      </Heading>
+      >
+        <Heading as="h3" size="lg" textAlign="center" mb={6}>
+          Review your delivery
+        </Heading>
 
-      <Flex w="full" justifyContent="space-between">
-        <VStack spacing={4} align="stretch" w="60%">
+        <Flex w={"80%"} justifyContent="center">
+          <VStack spacing={4} align="stretch" w="100%">
 
-          {/* pickup details */}
-          <Box
-            bg="white"
-            p={6}
-            borderRadius="md"
-            boxShadow="lg"
-            maxW="600px"
-            w="full"
-          >
+            <Box
+              bg="white"
+              p={6}
+              borderRadius="md"
+              boxShadow="md"
+              w="90%"
+            >
 
-            <VStack spacing={3} align="start" as="div">
-              <Heading as="h3" size="lg">
-                Pickup details
-              </Heading>
+              <VStack spacing={3} align="start" as="div">
+                <Heading as="h3" size="md" mb={2}>
+                  Pickup details
+                </Heading>
 
-              <Flex align="center" mb={4}>
-                <Icon as={FaMapMarkerAlt} w={5} h={5} color="black" mr={2} />
-                <Text fontSize="md">Nittambuwa</Text>
-              </Flex>
+                <Flex align="center" mb={4}>
+                  <Icon as={FaMapMarkerAlt} w={5} h={5} color="black" mr={2} />
+                  <Text fontSize="md">Nittambuwa</Text>
+                </Flex>
 
-              <VStack spacing={2} w="400px">
-                <Input placeholder="Business / building name" />
-                <Input placeholder="Apt / Suite / Floor" />
+                <VStack spacing={2} w="400px">
+                  <Input placeholder="Business / building name" />
+                  <Input placeholder="Apt / Suite / Floor" />
+                </VStack>
               </VStack>
-              <Flex align="center" mt={6}>
-                <Icon as={FaUser} w={5} h={5} color="black" mr={2} />
-                <Text fontSize="md">Sayuru</Text>
-              </Flex>
+            </Box>
 
-            </VStack>
-          </Box>
+            <Box
+              bg="white"
+              p={6}
+              borderRadius="md"
+              boxShadow="md"
+              w={"90%"}
+            >
+              <VStack spacing={2} align="start" as="div">
+                <Heading as="h3" size="md" mb={2}>
+                  Recipient details
+                </Heading>
+                <Flex align="center" mb={4}>
+                  <Icon as={FaMapMarkerAlt} w={5} h={5} color="black" mr={2} />
+                  <Text fontSize="md">Colombo</Text>
+                </Flex>
+                <Flex align="center">
+                  <Text width="150px">Name:</Text>
+                  <Input placeholder="Name of the recipient" w="350px" />
+                </Flex>
+                <Flex align="center">
+                  <Text width="150px">Address:</Text>
+                  <Input placeholder="Street , City , State" w="350px" />
+                </Flex>
+                <Flex align="center">
+                  <Text width="150px">Contact Number:</Text>
+                  <Input placeholder="Contact" w="350px" />
+                </Flex>
+              </VStack>
+            </Box>
 
-          {/* delivery details */}
-          <Box
-            bg="white"
-            p={6}
-            borderRadius="md"
-            boxShadow="lg"
-            maxW="600px"
-            w="full"
-          >
+            {/* guidelines */}
+            <Box
+              bg="white"
+              p={6}
+              borderRadius="md"
+              boxShadow="md"
+              w={"90%"}
+            >
+              <VStack spacing={2} align="stretch">
+                <Heading size="md" >
+                  Guidlines
+                </Heading>
 
-            <VStack spacing={2} align="start" as="div">
-              <Heading as="h3" size="lg" >
-                Recipient details
-              </Heading>
+                <Text size="md">General Guideliness:</Text>
+                //Small text
+                <Text size="xs" fontWeight={300}>1. Please make sure that the package is properly packed.</Text>
+                <Text size="xs" fontWeight={300}>2. Please make sure details are correct.</Text>
+                <Text size="xs" fontWeight={400}>Prohibited items:</Text>
+                <Text size="xs" fontWeight={300}>Prohibited items are Alcohol, medication, drugs, firearms, and dangerous or illegal items are prohibited.</Text>
 
-
-              <Flex align="center">
-                <Text width="150px">Name:</Text>
-                <Input placeholder="Name of the recipient" w="350px" />
-              </Flex>
-              <Flex align="center">
-                <Text width="150px">Address:</Text>
-                <Input placeholder="Street , City , State" w="350px" />
-              </Flex>
-              <Flex align="center">
-                <Text width="150px">Contact Number:</Text>
-                <Input placeholder="Contact" w="350px" />
-              </Flex>
-
-              <Button colorScheme="blue" variant="solid" >
-                Save Details
-              </Button>
-            </VStack>
-          </Box>
-
-          {/* guidelines */}
-          <Box
-            bg="white"
-            p={6}
-            borderRadius="md"
-            boxShadow="lg"
-            maxW="600px"
-            w="full"
-          >
-            <VStack spacing={2} align="stretch">
-              <Heading as="h3" size="lg" >
-                Guidlines
-              </Heading>
-
-              <Text >General Guideliness:</Text>
-              <h6>1.Listen actively to clients The first step in professional service delivery is to wholly understand your clients' needs and expectations. Active listening is a crucial part of this process. ...</h6>
-              <h6>2. Set clear expectations & deliverables ...</h6>
-              <h6>3. Create a service delivery plan ...</h6>
-            </VStack>
-          </Box>
-        </VStack>
-
-        {/* Order Summary */}
-        
-        <VStack align="stretch"  w="50%">
-          <Box
-            bg="white"
-            p={4}
-            borderRadius="md"
-            boxShadow="lg"
-            w="full"
-          >
-
-            <Heading as="h3" size="lg" p={4} >Order Summary</Heading>
-            <Text><strong>Product Name:</strong> Sample Product</Text>
-            <Text><strong>Description:</strong> This is a sample product description.</Text>
-            <Text><strong>Date and Time:</strong> June 17, 2024, 6:00 PM</Text>
-            <Text><strong>Price:</strong> $100.00</Text>
-
-            <Flex justifyContent="center" w="full">
-              <Image
-                src="https://via.placeholder.com/150"
-                alt="Passenger Image"
-                objectFit="cover"
-                borderRadius="md"
-              />
-            </Flex>
-            
-
-          </Box>
-          
-          <Button colorScheme="blue" variant="solid">
-            Book
-          </Button>
-
+              </VStack>
+            </Box>
           </VStack>
-        
-      </Flex>
-    </Flex>
-    
+
+          <VStack align="stretch" w="50%">
+            <Box
+              bg="white"
+              p={4}
+              borderRadius="md"
+              boxShadow="md"
+              w="full"
+            >
+
+              {/* <Heading as="h3" size="lg" p={4} >Order Summary</Heading> */}
+              <Flex justifyContent="center">
+                <Image
+                  src="https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&w=860&h=750&dpr=1"
+                  alt="Passenger Image"
+                  objectFit="cover"
+                  borderRadius="md"
+                />
+              </Flex>
+              <Text><strong>From:</strong> Kandy</Text>
+              <Text><strong>To:</strong> Badulla.</Text>
+              <Text><strong>Date and Time:</strong> June 17, 2024, 6:00 PM</Text>
+              <Text><strong>Price:</strong> $100.00</Text>
+
+            </Box>
+
+            <Button bg="black" variant="solid" color={'white'}>
+              Book
+            </Button>
+          </VStack>
+
+        </Flex>
+      </Flex >
+    </Box>
   );
 
 };
